c237292730de824a10107f91b3ec10b0b061ffa4
lucasjang01 <lucasjang01@gmail.com>
2023-11-14 16:00:59 -0300

    Cambio sender_queue y player por shared_ptr, conecto player con el protocolo y comandos de common, agrego los comandos create_game y join_game y agrego el metodo run con los parametros necesarios para esos comandos en command.h

---
+++ build/CMakeFiles/server.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/lucas/Desktop/taller/tpg-worms/Worms/src/common/liberror.cpp" "CMakeFiles/server.dir/src/common/liberror.cpp.o" "gcc" "CMakeFiles/server.dir/src/common/liberror.cpp.o.d"
+  "/home/lucas/Desktop/taller/tpg-worms/Worms/src/common/resolver.cpp" "CMakeFiles/server.dir/src/common/resolver.cpp.o" "gcc" "CMakeFiles/server.dir/src/common/resolver.cpp.o.d"
+  "/home/lucas/Desktop/taller/tpg-worms/Worms/src/common/resolvererror.cpp" "CMakeFiles/server.dir/src/common/resolvererror.cpp.o" "gcc" "CMakeFiles/server.dir/src/common/resolvererror.cpp.o.d"
+  "/home/lucas/Desktop/taller/tpg-worms/Worms/src/common/socket.cpp" "CMakeFiles/server.dir/src/common/socket.cpp.o" "gcc" "CMakeFiles/server.dir/src/common/socket.cpp.o.d"
+  "/home/lucas/Desktop/taller/tpg-worms/Worms/src/server/beam.cpp" "CMakeFiles/server.dir/src/server/beam.cpp.o" "gcc" "CMakeFiles/server.dir/src/server/beam.cpp.o.d"
+  "/home/lucas/Desktop/taller/tpg-worms/Worms/src/server/client_handler_thread.cpp" "CMakeFiles/server.dir/src/server/client_handler_thread.cpp.o" "gcc" "CMakeFiles/server.dir/src/server/client_handler_thread.cpp.o.d"
+  "/home/lucas/Desktop/taller/tpg-worms/Worms/src/server/commands/command.cpp" "CMakeFiles/server.dir/src/server/commands/command.cpp.o" "gcc" "CMakeFiles/server.dir/src/server/commands/command.cpp.o.d"
+  "/home/lucas/Desktop/taller/tpg-worms/Worms/src/server/game_manager.cpp" "CMakeFiles/server.dir/src/server/game_manager.cpp.o" "gcc" "CMakeFiles/server.dir/src/server/game_manager.cpp.o.d"
+  "/home/lucas/Desktop/taller/tpg-worms/Worms/src/server/player_sender_thread.cpp" "CMakeFiles/server.dir/src/server/player_sender_thread.cpp.o" "gcc" "CMakeFiles/server.dir/src/server/player_sender_thread.cpp.o.d"
+  "/home/lucas/Desktop/taller/tpg-worms/Worms/src/server/server_accept_thread.cpp" "CMakeFiles/server.dir/src/server/server_accept_thread.cpp.o" "gcc" "CMakeFiles/server.dir/src/server/server_accept_thread.cpp.o.d"
+  "/home/lucas/Desktop/taller/tpg-worms/Worms/src/server/server_game.cpp" "CMakeFiles/server.dir/src/server/server_game.cpp.o" "gcc" "CMakeFiles/server.dir/src/server/server_game.cpp.o.d"
+  "/home/lucas/Desktop/taller/tpg-worms/Worms/src/server/server_games_handler.cpp" "CMakeFiles/server.dir/src/server/server_games_handler.cpp.o" "gcc" "CMakeFiles/server.dir/src/server/server_games_handler.cpp.o.d"
+  "/home/lucas/Desktop/taller/tpg-worms/Worms/src/server/server_main.cpp" "CMakeFiles/server.dir/src/server/server_main.cpp.o" "gcc" "CMakeFiles/server.dir/src/server/server_main.cpp.o.d"
+  "/home/lucas/Desktop/taller/tpg-worms/Worms/src/server/server_player.cpp" "CMakeFiles/server.dir/src/server/server_player.cpp.o" "gcc" "CMakeFiles/server.dir/src/server/server_player.cpp.o.d"
+  "/home/lucas/Desktop/taller/tpg-worms/Worms/src/server/server_protocol.cpp" "CMakeFiles/server.dir/src/server/server_protocol.cpp.o" "gcc" "CMakeFiles/server.dir/src/server/server_protocol.cpp.o.d"
+  "/home/lucas/Desktop/taller/tpg-worms/Worms/src/server/server_server.cpp" "CMakeFiles/server.dir/src/server/server_server.cpp.o" "gcc" "CMakeFiles/server.dir/src/server/server_server.cpp.o.d"
+  "/home/lucas/Desktop/taller/tpg-worms/Worms/src/server/world.cpp" "CMakeFiles/server.dir/src/server/world.cpp.o" "gcc" "CMakeFiles/server.dir/src/server/world.cpp.o.d"
+  "/home/lucas/Desktop/taller/tpg-worms/Worms/src/server/worm.cpp" "CMakeFiles/server.dir/src/server/worm.cpp.o" "gcc" "CMakeFiles/server.dir/src/server/worm.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")

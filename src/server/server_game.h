5e743be49b13c25a6caff2036af3adacf71969ad
adrian <agalarza@fi.uba.ar>
2023-11-20 13:53:11 -0300

    Cambio el game loop del server

--- src/server/server_game.h
+++ src/server/server_game.h
@@ -6,15 +6,24 @@
 #include <map>
 #include <mutex>
 
+#include <chrono>
+#include <cmath>
+
 #include "../common/game_state.h"
 #include "../common/command.h"
 #include "../common/thread.h"
 #include "../common/queue.h"
+#include "../common/constant_rate_loop.h"
 #include "game_manager.h"
 #include "broadcaster.h"
 
 #define MAX_PLAYERS 2
 #define MS_PER_UPDATE 10
+#define RATE 0.01
+
+typedef duration<float, duration<float>> dur_ms;
+typedef time_point<steady_clock, milliseconds> time_p_ms;
+typedef duration<float> dur_f;
 
 //class Command;
 
@@ -27,7 +36,9 @@ private:
   int game_id;
   bool keep_playing = true;
   GameManager game_manager;
-  double rate = 0.01;
+  std::chrono::duration<float> rate = std::chrono::duration<float>((float)RATE);
+  
+  
 
 public:
   explicit Game(int &game_id);
